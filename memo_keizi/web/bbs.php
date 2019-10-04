8ad0622b5fff4d62191ac600ba9e2f1f843e3aef
atsushi-815 <atsushi586@yahoo.co.jp>
2019-10-04 14:14:53 +0900

    html部分作成

---
+++ memo_keizi/web/bbs.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <title>一言掲示板</title>
+    </head>
+    <body>
+        <h1>一言掲示板</h1>
+
+        <form action="bbs.php" method="post">
+            名前： <input type="text" name="name" /><br />
+            一言： <input type="text" name="comment" size="60" /><br />
+            <input type="submit" name="submit" value="送信" />
+        </form>
+    </body>
+</html>
