ec9154cc394c2317c7020357c745dee7527b6574
atsushi-815 <atsushi586@yahoo.co.jp>
2019-10-09 22:45:41 +0900

    10/09

--- memo_keizi/web/bbs.php
+++ memo_keizi/web/bbs.php
@@ -72,6 +72,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
         <form action="bbs.php" method="post">
             <?php if (count($errors)): ?>
             <ul class="error_list">
+                <?php foreach ($errors as $error): ?>
+                <li>
+                    <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+            <?php endif; ?>
             名前： <input type="text" name="name" /><br />
             一言： <input type="text" name="comment" size="60" /><br />
             <input type="submit" name="submit" value="送信" />
