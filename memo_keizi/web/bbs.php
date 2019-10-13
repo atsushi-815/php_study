ce29a95d589d18a2fd33bca55698654d57005b30
atsushi-815 <atsushi586@yahoo.co.jp>
2019-10-13 23:02:21 +0900

    10/13修正

--- memo_keizi/web/bbs.php
+++ memo_keizi/web/bbs.php
@@ -90,6 +90,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 
         <?php
         //投稿された内容を取得するSQLを作成して結果を取得
-        $sql
+        $sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
+        $result = mysql_query($sql, $link);
+        ?>
+
+        <?php if ($result !== false && mysql_num_rows($result)): ?>
+        <ul>
+            <?php while ($post = nysql_fetch_assoc($result)): ?>
+            <li>
+                <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>:
+                <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
+                - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
+            </li>
+            <?php endwhile; ?>
+        </ul>
+        <?php endif; ?>
+
+        <?php
+        // 取得結果を解放して接続を閉じる
+        mysql_free_result($result);
+        mysql_close($link);
+        ?>
     </body>
 </html>
