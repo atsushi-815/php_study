f74ec4b78692e39ddf0b7c55f9e60f8ec04ae802
atsushi-815 <atsushi586@yahoo.co.jp>
2019-10-15 23:12:19 +0900

    修正

--- memo_keizi/web/bbs.php
+++ memo_keizi/web/bbs.php
@@ -63,8 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 }
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1-transitional.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN""http://www.w3.org/TR/xhtml1-transitional.dtd">
 <html>
     <head>
         <title>一言掲示板</title>
